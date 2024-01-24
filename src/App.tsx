77cf54a36cc435b36b6667b57557a9f5c9f8195e
Fabio Rose <fabiorose1811@gmail.com>
2024-01-24 17:29:14 +0100

    installed kanva + router and updated layout for react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,16 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
 import '@mantine/core/styles.css';
-import {Button, FileButton, FileInput, Flex, Group, NativeSelect, Stack, Text} from "@mantine/core";
-import JsonFileInput from "./components/JsonFileInput";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Settings from "./pages/Settings";
 
 export default function App() {
-  const [inputDevices, setInputDevices] = useState([]);
-  const [file, setFile] = useState<File | null>(null);
-
-  useEffect(() => {
-    fetch("http://localhost:5001/input-devices")
-      .then((response) => response.json())
-      .then((data) => {
-        setInputDevices(data["devices"])
-      })
-      .catch((err) => {
-        console.log(err.message)
-      })
-  }, []);
-
-
-  useEffect(() => {
-    const uploadFile = async (): Promise<void> => {
-      if (file) {
-        try {
-          const formData = new FormData();
-          formData.append("file", file);
-
-          const response = await fetch("http://localhost:5001/upload-calib", {
-            method: "POST",
-            body: formData,
-          });
-
-          if (response.ok) {
-            console.log("Erfolgreich hochgeladen");
-          } else {
-            console.log("Fehler", response.statusText);
-          }
-        } catch (error) {
-          console.error("Fehler", error);
-        }
-      }
-    }
-
-    uploadFile();
-  }, [file]);
-
   return (
-    <Stack
-      h="100vh"
-    >
-      <NativeSelect label="Eingabegerät" data={inputDevices} />
-      <Group>
-        <FileButton onChange={setFile} accept={"application/json"}>
-          {(props) => <Button {...props}>Kalibrierungsdatei hochladen</Button>}
-        </FileButton>
-      </Group>
-      {file && (
-        <Text size="sm" ta="left" mt="sm">
-          {file.name}
-        </Text>
-      )}
-    </Stack>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Settings />} />
+      </Routes>
+    </Router>
   );
 }
 
