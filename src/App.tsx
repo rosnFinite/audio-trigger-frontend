ae0a84c4e8c2351f5bc4059da9630f81fabe2d45
Fabio Rose <fabiorose1811@gmail.com>
2024-01-25 14:52:34 +0100

    initial react-konva setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './styles/App.css';
 import '@mantine/core/styles.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Settings from "./views/Settings";
+import VoiceField from "./views/VoiceField";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Settings />} />
+        <Route path="/field" element={<VoiceField />} />
       </Routes>
     </Router>
   );
