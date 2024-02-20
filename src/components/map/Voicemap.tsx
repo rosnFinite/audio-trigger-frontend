c78e2126a312082ee3c2c3186ab1e17c96605240
Fabio Rose <fabiorose1811@gmail.com>
2024-02-20 17:19:43 +0100

    updated voicemap axis

--- src/components/map/Voicemap.tsx
+++ src/components/map/Voicemap.tsx
@@ -3,36 +3,22 @@ import { ApexOptions } from "apexcharts";
 import Chart from "react-apexcharts";
 import { Series } from "../../types/Voicemap.types";
 
-const series: Series[] = [
-  {
-    name: "Monday",
-    data: [31, 40, 28, 51, 42, 109, 100],
-  },
-  {
-    name: "Tuesday",
-    data: [11, 32, 45, 32, 34, 52, 41],
-  },
-  {
-    name: "Wednesday",
-    data: [55, 32, 20, 44, 55, 41, 60],
-  },
-  {
-    name: "Thursday",
-    data: [91, 56, 55, 40, 40, 79, 80],
-  },
-  {
-    name: "Friday",
-    data: [91, 56, 55, 40, 40, 79, 80],
-  },
-  {
-    name: "Saturday",
-    data: [91, 56, 55, 40, 40, 79, 80],
-  },
-  {
-    name: "Sunday",
-    data: [91, 56, 55, 40, 40, 79, 80],
-  },
-];
+// create a function that a returns a array of objects with key name, which contains an increasing number from 35 to 115 in which step size of 5
+// and key data, which contains an array of 33 random numbers between 10 and 90
+function generateMockData(numYLabels: number, numXLabels: number) {
+  let i = 0;
+  const series = [];
+  while (i < numYLabels) {
+    series.push({
+      name: (35+i*5).toString(),
+      data: Array.from({ length: numXLabels }, () => 0)
+    });
+    i++;
+  }
+  return series;
+}
+
+const series: Series[] = generateMockData(17, 33);
 
 const options: ApexOptions = {
   chart: {
@@ -42,17 +28,47 @@ const options: ApexOptions = {
   dataLabels: {
     enabled: false
   },
+  grid: {
+    show: true,
+    borderColor: '#90A4AE',
+    strokeDashArray: 0,
+    xaxis: {
+      lines: {
+        show: true
+      }
+    },
+    yaxis: {
+      lines: {
+        show: true
+      }
+    }
+  },
   colors: ["#008FFB"],
   title: {
     text: "Stimmfeld"
   },
+  plotOptions: {
+    heatmap: {
+      radius:5
+    }
+  },
+  xaxis:{
+    title: {
+      text: "Frequenz [Hz]"
+    },
+  },
+  yaxis: {
+    title: {
+      text: "dB(A)"
+    }
+  }
 };
 
 export default function Voicemap() {
   return (
-    <Container>
+    <Container ml={0} mr={30} fluid>
       <Chart 
-        options={options} series={series} type="heatmap" height={350} />
+        options={options} series={series} type="heatmap" height={"1000"} width={"100%"} />
     </Container>
   );
 }
