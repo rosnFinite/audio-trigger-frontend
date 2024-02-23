788f28e7c5017c6cb4537f9bcb33ac3c14753369
Fabio Rose <rosef@hochschule-trier.de>
2024-02-23 12:24:51 +0100

    Added control panel for trigger

--- src/views/VoiceField.tsx
+++ src/views/VoiceField.tsx
@@ -4,6 +4,9 @@ import {Socket} from "socket.io-client";
 import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { initialize } from "../components/map/voicemapDataSlice";
 import NivoVoicemap from "../components/map/NivoVoicemap";
+import { Blockquote, Button, Center, Divider, Group, Stack, Title, Tooltip } from "@mantine/core";
+import { TbInfoCircle, TbPlayerRecord, TbPlayerStop, TbProgressX } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 interface VoiceFieldProps {
   socket: Socket
@@ -26,6 +29,38 @@ export default function VoiceField({socket}: VoiceFieldProps) {
   }, []);
   
   return (
-    <NivoVoicemap />
+    <Stack h={"100%"}>
+      <Title order={2}>Stimmfeld</Title>
+      <Divider my="xs"/>
+      <Blockquote color='blue' icon={<TbInfoCircle size={"25"}/>} mt="xs" pt={10} pb={10}>
+        Über die Schaltfläche 'Patientenansicht' wird ein neuer Tab geöffnet, in dem die Patientenansicht angezeigt wird.
+        Diese beinhaltet ausschließlich das Stimmfeld und keine weiteren Schaltflächen.
+      </Blockquote>
+      <Center>
+        <Link to="/patient-view">
+          <Button>
+            Patientenansicht
+          </Button>
+        </Link>
+      </Center>
+      <Group justify="center" gap={"xs"}>
+        <Tooltip label="Startet den Triggerprozess">
+          <Button variant="outline" leftSection={<TbPlayerRecord size={"1.5rem"} />}>
+            Start
+          </Button>
+        </Tooltip>
+        <Tooltip label="Stoppt den Triggerprozess">
+          <Button variant="outline" leftSection={<TbPlayerStop size={"1.5rem"} />}>
+            Stop
+          </Button>
+        </Tooltip>
+        <Tooltip label="Setzt das gesamte Stimmfeld zurück">
+          <Button variant="outline" leftSection={<TbProgressX size={"1.5rem"}/>}>
+            Reset
+          </Button>
+        </Tooltip>
+      </Group>
+      <NivoVoicemap />
+    </Stack>
   )
 }
