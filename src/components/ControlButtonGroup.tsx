71cd98276f234f26e758835ad97b360f24de02db
Fabio Rose <rosef@hochschule-trier.de>
2024-02-26 13:15:36 +0100

    added tooltip group

--- src/components/ControlButtonGroup.tsx
+++ src/components/ControlButtonGroup.tsx
@@ -7,37 +7,42 @@ export default function ControlButtonGroup() {
   const dispatch = useAppDispatch();
 
   return (
-    <Group justify="center" gap={"xs"}>
-      <Tooltip label="Startet den Triggerprozess">
-        <Button 
-          variant={status.recorder==="ready" ? "filled" : "outline"} 
-          color="green" 
-          leftSection={<TbPlayerRecord size={"25"} />}
-          onClick={() => {dispatch({type: "settings/updateStatus", payload: {"recorder": "online", "trigger": "online"}})}}
-        >
-          Start
-        </Button>
-      </Tooltip>
-      <Tooltip label="Stoppt den Triggerprozess">
-        <Button 
-          variant={status.trigger==="online" ? "filled" : "outline"} 
-          color="red" 
-          leftSection={<TbPlayerStop size={"25"} />}
-          onClick={() => {dispatch({type: "settings/updateStatus", payload: {"recorder": "ready", "trigger": "ready"}})}}
-        >
-          Stop
-        </Button>
-      </Tooltip>
-      <Tooltip label="Stoppt den Triggerprozess und setzt das gesamte Stimmfeld zurück">
-        <Button 
-          variant={status.trigger==="online" ? "filled" : "outline"}
-          color="red" 
-          leftSection={<TbProgressX size={"25"}/>}
-          onClick={() => {dispatch({type: "settings/updateStatus", payload: {"recorder": "ready", "trigger": "ready"}})}}
-        >
-          Reset
-        </Button>
-      </Tooltip>
-    </Group>
+    <Tooltip.Group>
+      <Group justify="center" gap={"xs"}>
+        <Tooltip label="Startet den Triggerprozess" withArrow>
+          <Button 
+            variant={status.recorder==="ready" ? "filled" : "outline"} 
+            color="green" 
+            leftSection={<TbPlayerRecord size={"25"} />}
+            disabled={status.recorder==="online"}
+            onClick={() => {dispatch({type: "settings/updateStatus", payload: {"recorder": "online", "trigger": "online"}})}}
+          >
+            Start
+          </Button>
+        </Tooltip>
+        <Tooltip label="Stoppt den Triggerprozess" withArrow>
+          <Button 
+            variant={status.trigger==="online" ? "filled" : "outline"} 
+            color="red" 
+            leftSection={<TbPlayerStop size={"25"} />}
+            disabled={status.trigger==="ready"}
+            onClick={() => {dispatch({type: "settings/updateStatus", payload: {"recorder": "ready", "trigger": "ready"}})}}
+          >
+            Stop
+          </Button>
+        </Tooltip>
+        <Tooltip label="Stoppt den Triggerprozess und setzt das gesamte Stimmfeld zurück" withArrow>
+          <Button 
+            variant={status.trigger==="online" ? "filled" : "outline"}
+            color="red" 
+            leftSection={<TbProgressX size={"25"}/>}
+            disabled={status.trigger==="ready"}
+            onClick={() => {dispatch({type: "settings/updateStatus", payload: {"recorder": "ready", "trigger": "ready"}})}}
+          >
+            Reset
+          </Button>
+        </Tooltip>
+      </Group>
+    </Tooltip.Group>
   );
 };
