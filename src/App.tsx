1da545096b853126d0fad9908863b418a3c10c91
Fabio Rose <fabiorose1811@gmail.com>
2024-03-14 13:53:41 +0100

    small chnage to dependency array of useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import Settings from "./views/Settings";
 import VoiceField from "./views/VoiceField";
-import {socket} from "./socket";
-import Patient from './views/Patient';
-import { useAppDispatch, useAppSelector } from './redux/hooks';
-import { persistor } from './redux/store';
-import Recordings from './views/Recordings';
+import { socket } from "./socket";
+import Patient from "./views/Patient";
+import { useAppDispatch, useAppSelector } from "./redux/hooks";
+import { persistor } from "./redux/store";
+import Recordings from "./views/Recordings";
 
 export default function App() {
   const settings = useAppSelector((state) => state.settings.values);
@@ -15,32 +15,32 @@ export default function App() {
   // handles registering of web client to socketio backend and sets the audio client sid
   useEffect(() => {
     socket.on("connect", () => {
-      socket.emit("registerClient", {type: "web"});
+      socket.emit("registerClient", { type: "web" });
     });
-    socket.on("clients", (clients: {sid: string, type: string}[]) => {
+    socket.on("clients", (clients: { sid: string; type: string }[]) => {
       console.log("clients event", clients);
       // find audio client in clients array
-      const audioClient = clients.find(item => item.type === "audio");
+      const audioClient = clients.find((item) => item.type === "audio");
       // if audio client is connected
       if (audioClient) {
         const { sid } = audioClient;
         if (sid !== settings.sid) {
           // clear persisted state if sid of connected audio client changes
           persistor.purge();
-          dispatch({type: "settings/SET_CLIENT_SID", payload: {sid}});
-          dispatch({type: "voicemap/INITIALIZE"});
+          dispatch({ type: "settings/SET_CLIENT_SID", payload: { sid } });
+          dispatch({ type: "voicemap/INITIALIZE" });
         }
       } else {
         // clear persisted state if audio client is disconnected or not found
-        dispatch({type: "settings/SET_CLIENT_SID", payload: {sid: ""}});
-        dispatch({type: "voicemap/INITIALIZE"});
+        dispatch({ type: "settings/SET_CLIENT_SID", payload: { sid: "" } });
+        dispatch({ type: "voicemap/INITIALIZE" });
       }
     });
-  }, [socket]);
+  }, []);
 
-  return(
+  return (
     <Routes>
-      <Route path="/" element={<Settings socket={socket}/>} />
+      <Route path="/" element={<Settings socket={socket} />} />
       <Route path="/stimmfeld">
         <Route index element={<VoiceField socket={socket} />} />
         <Route path="patientenansicht" element={<Patient socket={socket} />} />
@@ -49,4 +49,3 @@ export default function App() {
     </Routes>
   );
 }
-
