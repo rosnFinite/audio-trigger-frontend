268dff75834b68bde7519fd88e984a84a9202929
Fabio Rose <fabiorose1811@gmail.com>
2024-03-18 15:48:49 +0100

    updated voicefield view to include Recordings

--- src/components/recording/Recording.tsx
+++ src/components/recording/Recording.tsx
@@ -1,9 +1,65 @@
-import { Card } from "@mantine/core";
+import {
+  Card,
+  Container,
+  Image,
+  Flex,
+  Text,
+  Button,
+  Group,
+} from "@mantine/core";
 
-export default function Recording() {
+export default function Recording({
+  freqBereich,
+  dbBereich,
+  qScore,
+  saveLocation,
+  timestamp,
+}: {
+  freqBereich: string;
+  dbBereich: string;
+  qScore: string;
+  saveLocation: string;
+  timestamp: string;
+}) {
   return (
-    <Card>
-      <h1>Recording</h1>
+    <Card
+      radius="md"
+      mb={8}
+      shadow="sm"
+      withBorder
+      h={150}
+      pt={0}
+      pl={0}
+      pr={0}
+    >
+      <Flex>
+        <Image src="/temp/TEST_C001H001S0002000001.jpg" h={150} w={150} />
+        <Container ml={0} mt={10} h={"100%"}>
+          <Group>
+            <Text fw={700}>Frequenzbereich [Hz]:</Text>
+            <Text>{freqBereich}</Text>
+          </Group>
+          <Group>
+            <Text fw={700}>Dezibelbereich [db(A)]:</Text>
+            <Text>{dbBereich}</Text>
+          </Group>
+          <Group>
+            <Text fw={700}>Q-Score:</Text>
+            <Text>{qScore}</Text>
+          </Group>
+          <Group>
+            <Text fw={700}>Speicherort:</Text>
+            <Text>{saveLocation}</Text>
+          </Group>
+          <Group>
+            <Text fw={700}>Zeitstempel:</Text>
+            {timestamp}
+          </Group>
+        </Container>
+        <Button h="100%" color="red">
+          Löschen
+        </Button>
+      </Flex>
     </Card>
   );
 }
--- src/views/VoiceField.tsx
+++ src/views/VoiceField.tsx
@@ -1,33 +1,130 @@
-import {Socket} from "socket.io-client";
 import NivoVoicemap from "../components/map/NivoVoicemap";
-import { Blockquote, Button, Center, Divider, Stack, Title } from "@mantine/core";
+import {
+  Blockquote,
+  Button,
+  Center,
+  Divider,
+  ScrollArea,
+  Stack,
+  Title,
+} from "@mantine/core";
 import { TbInfoCircle, TbSwipe } from "react-icons/tb";
 import { Link } from "react-router-dom";
 import ControlButtonGroup from "../components/ControlButtonGroup";
 import Layout from "../components/Layout/Layout";
 import { SocketProp } from "../types/SocketProp.types";
+import Recording from "../components/recording/Recording";
 
-
-export default function VoiceField({socket}: SocketProp) {
+export default function VoiceField({ socket }: SocketProp) {
   return (
     <Layout>
       <Stack h={"100%"}>
         <Title order={2}>Stimmfeld</Title>
-        <Divider my="xs"/>
-        <Blockquote color='blue' icon={<TbInfoCircle size={"25"}/>} mt="xs" pt={10} pb={10}>
-          Über die Schaltfläche 'Patientenansicht' wird ein neuer Tab geöffnet, in dem die Patientenansicht angezeigt wird.
-          Diese beinhaltet ausschließlich das Stimmfeld und keine weiteren Schaltflächen.
+        <Divider my="xs" />
+        <Blockquote
+          color="blue"
+          icon={<TbInfoCircle size={"25"} />}
+          mt="xs"
+          pt={10}
+          pb={10}
+        >
+          Über die Schaltfläche 'Patientenansicht' wird ein neuer Tab geöffnet,
+          in dem die Patientenansicht angezeigt wird. Diese beinhaltet
+          ausschließlich das Stimmfeld und keine weiteren Schaltflächen.
         </Blockquote>
         <Center>
           <Link to="/stimmfeld/patientenansicht" target="_blank">
-            <Button rightSection={<TbSwipe />}>
-              Patientenansicht
-            </Button>
+            <Button rightSection={<TbSwipe />}>Patientenansicht</Button>
           </Link>
         </Center>
-        <ControlButtonGroup socket={socket}/>
-        <NivoVoicemap socket={socket}/>
+        <ControlButtonGroup socket={socket} />
+        <NivoVoicemap socket={socket} />
+        <Title order={2}>Aufnahmen</Title>
+        <Divider my="xs" />
+        <Blockquote
+          color="blue"
+          icon={<TbInfoCircle size={"25"} />}
+          mt="xs"
+          pt={10}
+          pb={10}
+        >
+          Hier können die vom Trigger aufgezeichneten Audio, sowie Bildaufnahmen
+          analysiert und selektiert werden. Sollten Aufnahmen nicht einer
+          vordefinierten Qualität entsprechen besteht hier die Möglichkeit diese
+          zu entfernen und eine erneute Aufnahme dieser zu starten.
+        </Blockquote>
+        <ScrollArea
+          h={450}
+          type="auto"
+          offsetScrollbars
+          scrollbarSize={8}
+          scrollHideDelay={1500}
+        >
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+          <Recording
+            freqBereich="113.66-146.33"
+            dbBereich="45-50"
+            qScore="26.34"
+            saveLocation="C:/user/images/fksdjhfsd.jpg"
+            timestamp="2024-03-17 14:22:12"
+          />
+        </ScrollArea>
       </Stack>
     </Layout>
-  )
+  );
 }
