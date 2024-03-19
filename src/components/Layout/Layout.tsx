572448cefa4c355de103d93d729b6c5e10d0e08b
Fabio Rose <fabiorose1811@gmail.com>
2024-03-19 01:44:37 +0100

    removed deleted view from navigation bar

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,27 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import './Layout.css';
-import '@mantine/core/styles.css';
-import { AppShell, Badge, Burger, Container, Flex, Group, LoadingOverlay, NavLink, Text } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { TbCameraSearch, TbAlpha, TbChartGridDots, TbMusicSearch, TbSettings } from 'react-icons/tb';
-import { useAppSelector } from '../../redux/hooks';
-import { useLocation } from 'react-router-dom';
-
-
+import React, { useEffect, useState } from "react";
+import "./Layout.css";
+import "@mantine/core/styles.css";
+import {
+  AppShell,
+  Badge,
+  Burger,
+  Container,
+  Flex,
+  Group,
+  LoadingOverlay,
+  NavLink,
+  Text,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  TbCameraSearch,
+  TbAlpha,
+  TbChartGridDots,
+  TbMusicSearch,
+  TbSettings,
+} from "react-icons/tb";
+import { useAppSelector } from "../../redux/hooks";
+import { useLocation } from "react-router-dom";
 
 export default function Layout(props: { children: React.ReactNode }) {
   const { pathname } = useLocation();
-  const [opened, {toggle}] = useDisclosure();
+  const [opened, { toggle }] = useDisclosure();
   const [recBadgeColor, setRecBadgeColor] = useState("red");
   const [trigBadgeColor, setTrigBadgeColor] = useState("red");
   const [loadingVisible, setLoadingVisible] = useState(false);
-  const status = useAppSelector((state) => state.settings.values.status)
+  const status = useAppSelector((state) => state.settings.values.status);
   const audioClientSID = useAppSelector((state) => state.settings.values.sid);
 
   useEffect(() => {
     console.log("Status: ", status);
     switch (status.recorder) {
-      case "running": 
+      case "running":
         setRecBadgeColor("lime");
         break;
       case "ready":
@@ -33,7 +47,7 @@ export default function Layout(props: { children: React.ReactNode }) {
     }
 
     switch (status.trigger) {
-      case "running": 
+      case "running":
         setTrigBadgeColor("lime");
         break;
       case "ready":
@@ -47,44 +61,40 @@ export default function Layout(props: { children: React.ReactNode }) {
 
   useEffect(() => {
     // Show loading overlay if no audio client is connected
-    setLoadingVisible(audioClientSID === "" ? true : false)
+    setLoadingVisible(audioClientSID === "" ? true : false);
   }, [audioClientSID]);
 
   return (
-    <AppShell header={{height:60}} navbar={{width: 180, breakpoint:"sm", collapsed: {mobile: !opened}}} padding="md">
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{ width: 180, breakpoint: "sm", collapsed: { mobile: !opened } }}
+      padding="md"
+    >
       <AppShell.Header>
         <Flex justify="flex-start" align="center">
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
           <Container w={70} ml={-5} mr={-10} mt={10}>
-            <TbChartGridDots size={"100%"} color='#1C7ED6'/>
+            <TbChartGridDots size={"100%"} color="#1C7ED6" />
           </Container>
-          <Text fw={500} size='xl'>
+          <Text fw={500} size="xl">
             Stimmfeldanalyse
           </Text>
           <Badge
-            rightSection={<TbAlpha />} 
-            size="sm" 
-            variant='gradient' 
-            gradient={{ from: 'blue', to: 'green', deg: 90 }} 
-            ml={10} 
+            rightSection={<TbAlpha />}
+            size="sm"
+            variant="gradient"
+            gradient={{ from: "blue", to: "green", deg: 90 }}
+            ml={10}
             mt={5}
           >
             Alpha
           </Badge>
-          <Group justify='flex-end' gap="xs" ml={"auto"} mr={"2%"}>
-            <Text size='xs'>{audioClientSID}</Text>
-            <Badge
-              size='md'
-              variant='filled'
-              color={recBadgeColor}
-            >
+          <Group justify="flex-end" gap="xs" ml={"auto"} mr={"2%"}>
+            <Text size="xs">{audioClientSID}</Text>
+            <Badge size="md" variant="filled" color={recBadgeColor}>
               Recorder
             </Badge>
-            <Badge 
-              size="md"
-              variant="filled"
-              color={trigBadgeColor}
-            >
+            <Badge size="md" variant="filled" color={trigBadgeColor}>
               Trigger
             </Badge>
           </Group>
@@ -92,15 +102,29 @@ export default function Layout(props: { children: React.ReactNode }) {
       </AppShell.Header>
 
       <AppShell.Navbar p="sm">
-        <NavLink href="/" label="Einstellungen" leftSection={<TbSettings />} active={pathname === "/"}/>
-        <NavLink href='/stimmfeld' label="Stimmfeld" leftSection={<TbMusicSearch />} active={pathname === "/stimmfeld"}/>
-        <NavLink href="/aufnahmen" label="Aufnahmen" leftSection={<TbCameraSearch />} active={pathname === "/aufnahmen"}/>
+        <NavLink
+          href="/"
+          label="Einstellungen"
+          leftSection={<TbSettings />}
+          active={pathname === "/"}
+        />
+        <NavLink
+          href="/stimmfeld"
+          label="Stimmfeld"
+          leftSection={<TbMusicSearch />}
+          active={pathname === "/stimmfeld"}
+        />
       </AppShell.Navbar>
       <AppShell.Main pos="relative">
-        <LoadingOverlay visible={loadingVisible} loaderProps={{ children: "Kein Audioclient verbunden. Bitte stelle eine Verbindung her..."}}/>
+        <LoadingOverlay
+          visible={loadingVisible}
+          loaderProps={{
+            children:
+              "Kein Audioclient verbunden. Bitte stelle eine Verbindung her...",
+          }}
+        />
         {props.children}
       </AppShell.Main>
     </AppShell>
   );
 }
-
