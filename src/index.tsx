fd9c0cc758d086fe3b8ef02a011056398bc3927e
Fabio Rose <fabiorose1811@gmail.com>
2024-03-21 11:55:46 +0100

    working dev prototype

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {MantineProvider} from "@mantine/core";
-import {Provider} from "react-redux";
-import {persistor, store} from "./redux/store";
-import {PersistGate} from "redux-persist/integration/react";
-import { BrowserRouter } from 'react-router-dom';
-import { Notifications } from '@mantine/notifications';
-import '@mantine/notifications/styles.css';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+import { Notifications } from "@mantine/notifications";
+import "@mantine/notifications/styles.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
         <MantineProvider>
-          <Notifications limit={5} position='bottom-right'/>
+          <Notifications limit={5} position="bottom-right" />
           <App />
         </MantineProvider>
       </PersistGate>
