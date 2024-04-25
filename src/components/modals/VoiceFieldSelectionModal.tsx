2501e495229948d8ee7eabf9acaf73ae9e368732
Fabio Rose <rosef@hochschule-trier.de>
2024-04-25 20:02:19 +0200

    initial voice field selection modal

--- src/components/modals/VoiceFieldSelectionModal.tsx
+++ src/components/modals/VoiceFieldSelectionModal.tsx
@@ -1,6 +1,6 @@
-import { Modal } from "@mantine/core";
+import { Modal, Stack, Image } from "@mantine/core";
 import { useAppSelector } from "../../redux/hooks";
-import Recording from "../recording/Recording";
+import { getRecordingBySelectionId } from "../../utils/selectionUtils";
 
 interface VoiceFieldSelectionModalProps {
   selectionId: string;
@@ -8,33 +8,19 @@ interface VoiceFieldSelectionModalProps {
   onClose: () => void;
 }
 
-const findRecordingBySelectionId = (recordings: RecordingStats[], selectionId: string, fieldBinNames: {freq: string[]; dba: string[]}) => {
-  const splittedId = selectionId.split(".");
-  const db = splittedId[0];
-  const freq = splittedId[1];
-  const dbBin = fieldBinNames.dba.findIndex((item) => item === db);
-  const freqBin = fieldBinNames.freq.findIndex((item) => item === freq);
-  const recording = recordings[
-    recordings.findIndex(
-      (item) => item.dbaBin === dbBin && item.freqBin === freqBin
-    )
-  ];
-  return recording;
-}
-
-
 export default function VoiceFieldSelectionModal({selectionId, opened, onClose}: VoiceFieldSelectionModalProps) {
   const recordings = useAppSelector((state) => state.voicemap.values.recordings);
   const fieldBinNames = useAppSelector((state) => state.voicemap.values.fieldBinNames);
 
-  const recording = findRecordingBySelectionId(recordings, selectionId, fieldBinNames);
+  const recording = getRecordingBySelectionId(recordings, selectionId, fieldBinNames);
 
   if (recording === undefined) {
     return null;
   }
   return (
     <Modal opened={opened} onClose={onClose} withCloseButton={false}>
-      <Recording freqBin={recording.freqBin} dbaBin={recording.dbaBin} acceptable={!recording.accepted} />
+      <Stack>
+      </Stack>
     </Modal>
   );
 }
