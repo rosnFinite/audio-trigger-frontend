8f7e6200c956a74023f530f31e7cc144404f54e4
Fabio Rose <rosef@hochschule-trier.de>
2024-05-17 12:03:36 +0200

    added functionality to accept and delete button on selection modal

--- src/components/modals/VoiceFieldSelectionModal.tsx
+++ src/components/modals/VoiceFieldSelectionModal.tsx
@@ -1,8 +1,9 @@
 import { Modal, Stack, Image, Group, Button } from "@mantine/core";
-import { useAppSelector } from "../../redux/hooks";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import { getRecordingBySelectionId } from "../../utils/selectionUtils";
 import { getRestBaseUrlForRecording } from "../../utils/apiUtils";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import SocketContext from "../../context/SocketContext";
 
 interface VoiceFieldSelectionModalProps {
   selectionId: string;
@@ -15,6 +16,8 @@ export default function VoiceFieldSelectionModal({
   opened,
   onClose,
 }: VoiceFieldSelectionModalProps) {
+  // needed for requesting removing recording from voice field on the backend
+  const socket = useContext(SocketContext);
   const recordings = useAppSelector(
     (state) => state.voicemap.values.recordings
   );
@@ -24,6 +27,7 @@ export default function VoiceFieldSelectionModal({
   const saveLocation = useAppSelector(
     (state) => state.settings.values.save_location
   );
+  const dispatch = useAppDispatch();
   const [frequency, setFrequency] = useState<string | null>(null);
   const [decibel, setDecibel] = useState<string | null>(null);
   const recording = getRecordingBySelectionId(
@@ -77,10 +81,44 @@ export default function VoiceFieldSelectionModal({
             </Button>
           ) : (
             <>
-              <Button size="lg" color="green" onClick={onClose}>
+              <Button
+                size="lg"
+                color="green"
+                onClick={() => {
+                  dispatch({
+                    type: "voicemap/ACCEPT_RECORDING",
+                    payload: {
+                      freqBin: recording.freqBin,
+                      dbaBin: recording.dbaBin,
+                    },
+                  });
+                  onClose();
+                }}
+              >
                 Akzeptieren
               </Button>
-              <Button size="lg" color="red" onClick={onClose}>
+              <Button
+                size="lg"
+                color="red"
+                onClick={() => {
+                  if (!socket) {
+                    console.error("Socket is not initialized");
+                    return;
+                  }
+                  socket.emit("remove_recording_request", {
+                    freqBin: recording.freqBin,
+                    dbaBin: fieldBinNames.dba.length - recording.dbaBin - 1,
+                  });
+                  dispatch({
+                    type: "voicemap/REMOVE_RECORDING",
+                    payload: {
+                      freqBin: recording.freqBin,
+                      dbaBin: recording.dbaBin,
+                    },
+                  });
+                  onClose();
+                }}
+              >
                 Löschen
               </Button>
             </>
