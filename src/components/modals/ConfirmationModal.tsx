9424b82e1c4b946693f47690e42943bf830e5075
Fabio Rose <rosef@hochschule-trier.de>
2024-06-07 02:28:20 +0200

    added component for creating confirmation modals

---
+++ src/components/modals/ConfirmationModal.tsx
@@ -0,0 +1,93 @@
+import {
+  Alert,
+  Button,
+  ButtonVariant,
+  Group,
+  MantineColor,
+  MantineSize,
+  Modal,
+} from "@mantine/core";
+import { TbInfoCircle } from "react-icons/tb";
+
+interface ConfirmationModalProps {
+  size?: MantineSize | (string & {}) | number;
+  title: string;
+  descriptionTitle: string;
+  descriptionText: string;
+  opened: boolean;
+  onAbort: () => void;
+  onConfirm: () => void;
+  confirmBtnText?: string;
+  abortBtnText?: string;
+  confirmBtnPosition?: "left" | "right";
+  confirmBtnColor?: MantineColor;
+  abortBtnColor?: MantineColor;
+  btnVariant?: ButtonVariant;
+}
+
+export default function ConfirmationModal({
+  size = "md",
+  confirmBtnText = "Bestätigen",
+  abortBtnText = "Abbrechen",
+  confirmBtnPosition = "left",
+  confirmBtnColor = "green",
+  abortBtnColor = "red",
+  btnVariant = "filled",
+  ...props
+}: ConfirmationModalProps) {
+  return (
+    <Modal
+      size={size}
+      opened={props.opened}
+      onClose={props.onAbort}
+      centered
+      title={props.title}
+    >
+      <Alert
+        variant="light"
+        color="red"
+        title={props.descriptionTitle}
+        icon={<TbInfoCircle size={25} />}
+      >
+        {props.descriptionText}
+      </Alert>
+      <Group mt={15} grow>
+        {confirmBtnPosition === "left" ? (
+          <>
+            <Button
+              variant={btnVariant}
+              color={confirmBtnColor}
+              onClick={props.onConfirm}
+            >
+              {confirmBtnText}
+            </Button>
+            <Button
+              variant={btnVariant}
+              color={abortBtnColor}
+              onClick={props.onAbort}
+            >
+              {abortBtnText}
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button
+              variant={btnVariant}
+              color={abortBtnColor}
+              onClick={props.onAbort}
+            >
+              {abortBtnText}
+            </Button>
+            <Button
+              variant={btnVariant}
+              color={confirmBtnColor}
+              onClick={props.onConfirm}
+            >
+              {confirmBtnText}
+            </Button>
+          </>
+        )}
+      </Group>
+    </Modal>
+  );
+}
